c0ea97b4b94910436e04c931d5ffd20d53870598
Vivianpcamargo <vivianpaulacamargo@gmail.com>
2022-05-17 18:49:19 -0300

    update: Aula 10

--- controllers.py
+++ controllers.py
@@ -1,12 +1,61 @@
 from app import app
-from flask import render_template
+from database.classes import Curso, Disciplina, Premio
+from flask import render_template, request
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        premios=Premio.listar()
+    )
 
 
-@app.route('/ola')
-def ola():
-    return '<h1>Olá Mundo Flask!</h1>'
+@app.route('/sobre')
+def sobre():
+    return render_template(
+        'sobre.html'
+    )
+
+
+@app.route('/entrar')
+def entrar():
+    return render_template(
+        'entrar.html'
+    )
+
+
+@app.route('/contato', methods=['GET', 'POST'])
+def contato():
+
+    if request.method == 'POST':
+        form = request.form
+        print(f'''
+        ++++ MENSAGEM ENVIADA ++++
+        -> Nome: {form.get('nome')}
+        -> E-mail: {form.get('email')}
+        -> Assunto: {form.get('assunto')}
+        -> Como Conheceu: {form.get('conheceu')}
+        -> Mensagem:
+        {form.get('mensagem')}
+        ''')
+
+    return render_template(
+        'contato.html'
+    )
+
+
+@app.route('/cursos/ads')
+def cursos():
+    objeto = Curso.obter('ads')
+    disciplinas = Disciplina.filtrar(objeto.sigla)
+    return render_template(
+        'curso.html',
+        curso=objeto,
+        disciplinas=disciplinas
+    )
+
+
+@app.context_processor
+def listar_cursos():
+    return dict(cursos_menu=Curso.listar())
