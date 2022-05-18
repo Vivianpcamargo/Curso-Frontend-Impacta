71ec3d27d74ea4ef85591f5fc289d8a92f5b3ea6
Vivianpcamargo <vivianpaulacamargo@gmail.com>
2022-05-18 16:31:03 -0300

    update: Aula 11

--- database/classes.py
+++ database/classes.py
@@ -17,12 +17,42 @@ class Curso(object):
         return f'{self.sigla} - {self.nome}'
 
     @classmethod
+    def alterar(cls, nome, sigla, tipo, descricao, coordenador,
+                duracao, diurno, noturno):
+        curso = cls(nome, sigla, tipo, descricao, coordenador,
+                    duracao, diurno, noturno)
+        erros = Curso.__validar(curso, True)
+
+        if len(erros) == 0:
+            original = Curso.obter(curso.sigla)
+            original.nome = curso.nome
+            original.sigla = curso.sigla
+            original.tipo = curso.tipo
+            original.descricao = curso.descricao
+            original.coordenador = curso.coordenador
+            original.duracao = curso.duracao
+            original.diurno = curso.diurno
+            original.noturno = curso.noturno
+
+        return erros
+
+    @classmethod
     def criar(cls, nome, sigla, tipo, descricao, coordenador,
               duracao, diurno, noturno):
-        Curso.__dados.append(
-            cls(nome, sigla, tipo, descricao, coordenador,
-                duracao, diurno, noturno)
-        )
+        curso = cls(nome, sigla, tipo, descricao, coordenador,
+                    duracao, diurno, noturno)
+        erros = Curso.__validar(curso)
+
+        if len(erros) == 0:
+            Curso.__dados.append(curso)
+
+        return erros
+
+    @classmethod
+    def remover(cls, sigla):
+        curso = Curso.obter(sigla)
+        if curso:
+            Curso.__dados.remove(curso)
 
     @classmethod
     def obter(cls, sigla):
@@ -33,6 +63,22 @@ class Curso(object):
     @classmethod
     def listar(cls):
         return Curso.__dados
+
+    @classmethod
+    def __validar(cls, curso, alteracao=False):
+        erros = []
+        if not curso.nome:
+            erros.append('Nome do curso é obrigatório!')
+
+        if not curso.sigla:
+            erros.append('Sigla do curso é obrigatória!')
+        elif not alteracao and Curso.obter(curso.sigla):
+            erros.append(f'A sigla {curso.sigla} já está sendo utilizada!')
+
+        if not curso.tipo:
+            erros.append('Tipo do curso é obrigatório!')
+
+        return erros
 
 
 class Disciplina(object):
@@ -88,3 +134,35 @@ class Premio(object):
     @classmethod
     def listar(cls):
         return Premio.__dados
+
+
+class Usuario(object):
+
+    __dados = []
+
+    def __init__(self, usuario, nome, email, senha):
+        self.usuario = usuario
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+    def __str__(self):
+        return self.nome
+
+    @classmethod
+    def autenticar(cls, usuario, senha):
+        usuario = Usuario.obter(usuario)
+        if usuario and usuario.senha == senha:
+            return usuario
+
+    @classmethod
+    def criar(cls, usuario, nome, email, senha):
+        Usuario.__dados.append(
+            cls(usuario, nome, email, senha)
+        )
+
+    @classmethod
+    def obter(cls, usuario):
+        for u in Usuario.__dados:
+            if u.usuario == usuario:
+                return u
